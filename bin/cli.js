7292d1556408bb6809ba7f708b2e37602241ab1c
Arne Backstein <arne.backstein@gmail.com>
2021-10-05 12:13:07 +0200

    ~ Move pages in modules folder

--- bin/cli.js
+++ bin/cli.js
@@ -12,7 +12,7 @@ const runCommand = command => {
   return true;
 }
 
-const repoName = process.argv[2] || "docs";
+const repoName = process.argv[2] != "undefined" ? process.argv[2] : "docs";
 const gitCheckoutCommand = `git clone --depth 1 https://github.com/arnebackstein/create-antora-starter.git ${repoName}`;
 const installDepsCommand = `cd ${repoName} && npm install`;
 const removeDotGitCommand = `rm -r .git`;
@@ -30,4 +30,4 @@ const removedDotGit = runCommand(removeDotGitCommand);
 if(!removedDotGit) process.exit(-1);
 
 console.log("Congratulations! You are ready. Follow the following commands to start");
-console.log(`cd ${repoName} && npm start`);
+console.log(`cd ${repoName} && npm run watcher`);
